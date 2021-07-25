cd7deea11cb12339ae31f770a7c09c52929128f5
Clint Ryan <cryan@dev-10.com>
2021-07-25 16:33:08 -0500

    added more functionality to delete agent

--- field-agent-ui/src/components/Agents.js
+++ field-agent-ui/src/components/Agents.js
@@ -30,27 +30,7 @@ function Agents() {
         getAgents(auth.user.token);
     }, [auth.user.token]);
 
-    const agentDeleteClickHandler = (agentId) => {
 
-        const init = {
-            method: 'DELETE', // GET by default
-            headers: {
-                'Authorization': `Bearer ${auth.user.token}`
-            }
-        };
-
-        fetch(`http://localhost:8080/api/agents/${agentId}`, init)
-            .then(response => {
-                if (response.status === 204) {
-                    getAgents(auth.user.token);
-                } else if (response.status === 404) {
-                    Promise.reject(`Agent ID ${agentId} not found`);
-                } else {
-                    Promise.reject('Something unexpected went wrong :)');
-                }
-            })
-            .catch(error => console.log(error));
-    };
 
     return (
         <div>
--- field-agent-ui/src/components/DeleteAgent.js
+++ field-agent-ui/src/components/DeleteAgent.js
@@ -1,5 +1,36 @@
+import {useContext} from "react";
+import AuthContext from "../AuthContext";
+import { Link, useHistory, useParams } from 'react-router-dom';
 
 function DeleteAgent() {
+
+    const auth = useContext(AuthContext);
+
+    const { id } = useParams();
+    const history = useHistory();
+
+    const agentDeleteClickHandler = (agentId) => {
+
+        const init = {
+            method: 'DELETE', // GET by default
+            headers: {
+                'Authorization': `Bearer ${auth.user.token}`
+            }
+        };
+
+        fetch(`http://localhost:8080/api/agents/${agentId}`, init)
+            .then(response => {
+                if (response.status === 204) {
+                    getAgents(auth.user.token);
+                } else if (response.status === 404) {
+                    Promise.reject(`Agent ID ${agentId} not found`);
+                } else {
+                    Promise.reject('Something unexpected went wrong :)');
+                }
+            })
+            .catch(error => console.log(error));
+    };
+
     return (
         <h2>DeleteAgent</h2>
     );
